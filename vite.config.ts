aeed4e1eaf9201f9934aa8918e07f9ebe75fbb2c
Joaquim <joaqsaav@gmail.com>
2026-05-19 14:27:54 +0200

    import tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 /// <reference types="vitest" />
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
